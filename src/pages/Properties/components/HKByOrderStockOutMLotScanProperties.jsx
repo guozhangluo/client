bd14bd33776aec2b8bba2804441e56a1d708c9b2
guozhangLuo <55224413+guozhangluo@users.noreply.github.com>
2022-07-28 17:33:14 +0800

    香港仓依订单出货
    香港仓依订单出货去箱号优化

--- src/pages/Properties/components/HKByOrderStockOutMLotScanProperties.jsx
+++ src/pages/Properties/components/HKByOrderStockOutMLotScanProperties.jsx
@@ -1,8 +1,5 @@
 import EntityScanProperties from "./entityProperties/EntityScanProperties";
 import MaterialLot from "../../../api/dto/mms/MaterialLot";
-import { Notification } from "../../../components/notice/Notice";
-import I18NUtils from "../../../api/utils/I18NUtils";
-import { i18NCode } from "../../../api/const/i18n";
 import HKWarehouseManagerRequest from "../../../api/gc/hongkong-warehouse-manager/HKWarehouseManagerRequest";
 import HKByOrderStockOutMLotScanTable from "../../../components/Table/gc/HKByOrderStockOutMLotScanTable";
 
@@ -34,18 +31,8 @@ export default class HKByOrderStockOutMLotScanProperties extends EntityScanPrope
           tableRrn: this.state.tableRrn,
           queryLotId: queryLotId,
           success: function(responseBody) {
-            let data = undefined;
-            let materialLot = responseBody.materialLot;
-            let materialLotId = materialLot.materialLotId;
-            if (materialLotId == "" || materialLotId == null || materialLotId == undefined){
-              data = new MaterialLot();
-              data[rowKey] = queryLotId;
-              data.setMaterialLotId(queryLotId);
-              data.errorFlag = true;
-              if (tableData.filter(d => d[rowKey] === data[rowKey]).length === 0) {
-                tableData.unshift(data);
-              }
-            } else {
+            let materialLotList = responseBody.materialLotList;
+            if (materialLotList && materialLotList.length > 0){
               let errorData = [];
               let trueData = [];
               tableData.forEach(data => {
@@ -55,9 +42,11 @@ export default class HKByOrderStockOutMLotScanProperties extends EntityScanPrope
                   trueData.push(data);
                 }
               });
-              if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
-                trueData.unshift(materialLot);
-              }
+              materialLotList.forEach(mLot =>{
+                if (tableData.filter(d => d[rowKey] === mLot[rowKey]).length === 0) {
+                  trueData.unshift(mLot);
+                }
+              });
               tableData = [];
               errorData.forEach(data => {
                 tableData.push(data);
@@ -65,7 +54,15 @@ export default class HKByOrderStockOutMLotScanProperties extends EntityScanPrope
               trueData.forEach(data => {
                 tableData.push(data);
               });
-            }
+            } else {
+              let data = new MaterialLot();
+              data[rowKey] = queryLotId;
+              data.setMaterialLotId(queryLotId);
+              data.errorFlag = true;
+              if (tableData.filter(d => d[rowKey] === data[rowKey]).length === 0) {
+                tableData.unshift(data);
+              }
+            } 
             self.setState({ 
               tableData: tableData,
               loading: false
