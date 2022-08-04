f21385e774cc790a6e9aaccae1db281aa0a62eb4
guozhangLuo <55224413+guozhangluo@users.noreply.github.com>
2022-08-04 10:54:07 +0800

    手持端入库位
    手持端入库位去箱号优化

--- src/pages/Properties/components/gc/wafer-issue/GCMobileMLotStockInProperties.jsx
+++ src/pages/Properties/components/gc/wafer-issue/GCMobileMLotStockInProperties.jsx
@@ -4,6 +4,7 @@ import { i18NCode } from "../../../../../api/const/i18n";
 import MobileProperties from "../../mobile/MobileProperties";
 import MessageUtils from "../../../../../api/utils/MessageUtils";
 import StockInManagerRequest from "../../../../../api/gc/stock-in/StockInManagerRequest";
+import RelayBoxStockInManagerRequest from "../../../../../api/gc/relayBox-stock-in/RelayBoxStockInManagerRequest";
 
 export default class GCMobileMLotStockInProperties extends MobileProperties{
 
@@ -17,34 +18,60 @@ export default class GCMobileMLotStockInProperties extends MobileProperties{
     handleSearch = () => {
         let self = this;
         const{table} = this.state;
-        let {rowKey,tableData} = this.state;
+        let {rowKey, tableData} = this.state;
         this.setState({loading: true});
         let data = "";
         let queryFields = this.form.state.queryFields;
         if (queryFields.length === 1) {
-            data = this.form.props.form.getFieldValue(queryFields[0].name);
+            data = this.form.props.form.getFieldValue(queryFields[0].name)
         }  
          // MB开头的则是中装箱号 扫描到MB开头的，则更新当前操作的物料批次的中装箱号
         let dataIndex = -1;
-        if (data.startsWith("MB") || data.startsWith("TB") || data.startsWith("CM") || data.startsWith("ZTB") || data.startsWith("ZCB")) {
-            tableData.forEach((materialLot) => {
-                tableData.map((data, index) => {
-                    if (data[rowKey] == materialLot[rowKey]) {
-                        dataIndex = index;
+        if ((data.startsWith("MB") || data.startsWith("TB") || data.startsWith("CM") || data.startsWith("ZTB") || data.startsWith("ZCB"))  && data.split(".").length == 1) {
+            if(tableData && tableData.length > 0){
+                tableData.forEach((materialLot) => {
+                    tableData.map((data, index) => {
+                        if (data[rowKey] == materialLot[rowKey]) {
+                            dataIndex = index;
+                        }
+                    });
+                    if(!materialLot.relaxBoxId){
+                        materialLot["relaxBoxId"] = data;
+                        tableData.splice(dataIndex, 1, materialLot);
                     }
                 });
-                if(!materialLot.relaxBoxId){
-                    materialLot["relaxBoxId"] = data;
-                    tableData.splice(dataIndex, 1, materialLot);
+                self.setState({ 
+                    tableData: tableData,
+                    loading: false,
+                });
+            } else {
+                let requestObject = {
+                    relayBoxId: data,
+                    success: function(responseBody) {
+                        let materialLots = responseBody.materialLots;
+                        materialLots.forEach((materialLot) => {
+                            if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
+                                tableData.unshift(materialLot);
+                            }
+                        });
+                        self.setState({ 
+                            tableData: tableData,
+                            loading: false,
+                        });
+                        self.form.resetFormFileds();
+                    },
+                    fail: function() {
+                        self.setState({ 
+                            tableData: tableData,
+                            loading: false
+                        });
+                        self.form.resetFormFileds();
+                    }
                 }
-            });
-            self.setState({ 
-                tableData: tableData,
-                loading: false,
-            });
+                RelayBoxStockInManagerRequest.sendQueryRelayBoxRequest(requestObject);
+            }
             self.form.resetFormFileds();
         } else if (data.startsWith("ZHJ ") || data.startsWith("HJ ") ) {
-            // ZHJ/HJ 开头的则是库位号 扫描到ZHJ/HJ开头的，则更新当前操作的物料批次的库位号
             tableData.forEach((materialLot) => {
                 tableData.map((data, index) => {
                     if (data[rowKey] == materialLot[rowKey]) {
@@ -62,15 +89,16 @@ export default class GCMobileMLotStockInProperties extends MobileProperties{
             });
             self.form.resetFormFileds();
         } else {
-            // 物料批次，需要请求后台做查询
             let requestObject = {
                 materialLotId: data,
                 tableRrn: table.objectRrn,
                 success: function(responseBody) {
-                    let materialLot = responseBody.materialLot;
-                    if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
-                        tableData.unshift(materialLot);
-                    }
+                    let materialLots = responseBody.materialLots;
+                    materialLots.forEach((materialLot) => {
+                        if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
+                            tableData.unshift(materialLot);
+                        }
+                    });
                     self.setState({ 
                         tableData: tableData,
                         loading: false,
@@ -85,7 +113,7 @@ export default class GCMobileMLotStockInProperties extends MobileProperties{
                     self.form.resetFormFileds();
                 }
             }
-            StockInManagerRequest.sendQueryByTableRrnAndLotIdRequest(requestObject);
+            RelayBoxStockInManagerRequest.sendQueryBoxRequest(requestObject);
         }
     }
     
@@ -123,9 +151,4 @@ export default class GCMobileMLotStockInProperties extends MobileProperties{
         });
         return flag;
     }
-
-
-    // buildTable = () => {
-    //     return <MobileMLotStockInTable ref={(dataTable) => { this.dataTable = dataTable }}  table={this.state.table} data={this.state.tableData} loading={this.state.loading} />
-    // }
 }
