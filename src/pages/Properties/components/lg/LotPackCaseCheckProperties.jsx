d8ec7c22889dcb3d6d7f4c3de856109cd0a0a82f
guozhangLuo <55224413+guozhangluo@users.noreply.github.com>
2022-09-05 16:11:55 +0800

    装箱检验
    扫描一个Lot自检

--- src/pages/Properties/components/lg/LotPackCaseCheckProperties.jsx
+++ src/pages/Properties/components/lg/LotPackCaseCheckProperties.jsx
@@ -1,45 +1,97 @@
+import { i18NCode } from "../../../../api/const/i18n";
 import TableManagerRequest from "../../../../api/table-manager/TableManagerRequest";
+import I18NUtils from "../../../../api/utils/I18NUtils";
+import { Notification } from "../../../../components/notice/Notice";
 import LotPackCaseCheckTable from "../../../../components/Table/lgTable/LotPackCaseCheckTable";
-import EntityDoubleScanProperties from "../entityProperties/EntityDoubleScanProperties";
+import EntityScanProperties from "../entityProperties/EntityScanProperties";
 
-export default class LotPackCaseCheckProperties extends EntityDoubleScanProperties{
+export default class LotPackCaseCheckProperties extends EntityScanProperties{
 
     static displayName = 'LotPackCaseCheckProperties';
     
+    constructor(props) {
+      super(props);
+      this.state = {...this.state, ...{checkAllFlag: false}};
+    }
+
     queryData = (whereClause) => {
+      debugger;
       const self = this;
-      let reloadTableData = false;
+      let {tableData, checkAllFlag} = this.state;
       let firstQueryField = self.form.state.queryFields[0];
-      if (whereClause.indexOf(firstQueryField.name) != -1) {
-          reloadTableData = true;
-      }
-      let requestObject = {
-        tableRrn: this.state.tableRrn,
-        whereClause: whereClause,
-        success: function(responseBody) {
-          let queryDatas = responseBody.dataList;
-          if (reloadTableData) {
+      let lotId = self.form.props.form.getFieldValue(firstQueryField.name);
+      if(tableData.length > 0 && checkAllFlag == false){
+        self.afterSecondQuery(lotId);
+      } else {
+        let requestObject = {
+          tableRrn: this.state.tableRrn,
+          whereClause: whereClause,
+          success: function(responseBody) {
+            let queryDatas = responseBody.dataList;
             if (queryDatas && queryDatas.length > 0) {
                 self.setState({ 
                   tableData: queryDatas,
                   loading: false,
-                  scanErrorFlag: false,
                   resetFlag:true,
                   selectedRowKeys: [],
                   selectedRows: [],
                 });
-                self.nextQueryNodeFocus();
                 self.form.resetFormFileds();
             } else {
-              self.showDataNotFound(reloadTableData);
+              self.showDataNotFound();
               self.resetData();
+              self.form.resetFormFileds();
+              self.form.state.queryFields[0].node.focus();
             }
-          } else {
-            self.afterSecondQuery(queryDatas);
           }
         }
+        TableManagerRequest.sendGetDataByRrnRequest(requestObject);
       }
-      TableManagerRequest.sendGetDataByRrnRequest(requestObject);
+    }
+
+    afterSecondQuery = (lotId) => {
+      let {rowKey, tableData, selectedRowKeys, selectedRows, checkAllFlag} = this.state;
+      let checkFlag = false;
+      tableData.forEach(data => {
+        if(data.lotId == lotId){
+          data.scaned = true;
+          checkFlag = true;
+          if (selectedRowKeys.indexOf(data[rowKey]) < 0) {
+            selectedRowKeys.push(data[rowKey]);
+            selectedRows.push(data);
+          }
+        }
+      });
+      
+      if(checkFlag){
+        if(selectedRows.length == tableData.length){
+          checkAllFlag = true;
+        }
+        this.setState({ 
+          selectedRowKeys: selectedRowKeys,
+          selectedRows: selectedRows,
+          tableData: tableData,
+          checkAllFlag: checkAllFlag,
+          resetFlag:false,
+          loading: false
+        });
+        this.form.state.queryFields[0].node.focus();
+        this.form.resetFormFileds();
+      } else {
+        this.showDataNotFound();
+        this.form.resetFormFileds();
+        this.form.state.queryFields[0].node.focus();
+      }
+    }
+
+    showDataNotFound = () => {
+      let self = this;
+      self.setState({ 
+        loading: false,
+      });
+      let data = self.form.props.form.getFieldValue(self.form.state.queryFields[0].name);
+      this.allFieldBlur();
+      Notification.showInfo(I18NUtils.getClientMessage(i18NCode.DataNotFound) + (data || ""));
     }
 
     buildTable = () => {
